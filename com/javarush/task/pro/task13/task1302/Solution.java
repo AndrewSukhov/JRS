aa04c697f6f787c37819dd29b93033b8a9e90d07
AndrewSukhov <igrok-cs@yandex.ru>
2021-09-18 18:47:08 +0300

    level 14. Коллекция HashSet. Проверка присутствия.Решение

--- com/javarush/task/pro/task13/task1302/Solution.java
+++ com/javarush/task/pro/task13/task1302/Solution.java
@@ -13,7 +13,11 @@ public class Solution {
     public static HashSet<String> words = new HashSet<>(asList("Если бы меня попросили выбрать язык на замену Java я бы не выбирал".split(" ")));
 
     public static void checkWords(String word) {
-        //напишите тут ваш код
+        if (words.contains(word)) {
+            System.out.println("Слово " + word + " есть в множестве");
+        } else {
+            System.out.println("Слова " + word + " нет в множестве");
+        }
     }
 
     public static void main(String[] args) {
