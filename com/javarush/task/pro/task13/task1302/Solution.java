d67bcf6cd9f21ae768cae5c3d667690de4818f6c
AndrewSukhov <igrok-cs@yandex.ru>
2021-09-18 18:18:11 +0300

    level 14. Коллекция HashSet. Проверка присутствия

---
+++ com/javarush/task/pro/task13/task1302/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task13.task1302;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static java.util.Arrays.asList;
+
+/* 
+Проверка присутствия
+*/
+
+public class Solution {
+    public static HashSet<String> words = new HashSet<>(asList("Если бы меня попросили выбрать язык на замену Java я бы не выбирал".split(" ")));
+
+    public static void checkWords(String word) {
+        //напишите тут ваш код
+    }
+
+    public static void main(String[] args) {
+        checkWords("JavaScript");
+        checkWords("Java");
+    }
+}
