632aa2a7320036f41a6400c46724f404ff36a0e0
AndrewSukhov <igrok-cs@yandex.ru>
2021-09-21 23:20:54 +0300

    level 14. ArrayList vs HashMap. Решение

--- com/javarush/task/pro/task13/task1312/Solution.java
+++ com/javarush/task/pro/task13/task1312/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.pro.task13.task1312;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /* 
@@ -13,17 +12,16 @@ public class Solution {
         System.out.println(getProgrammingLanguages());
     }
 
-    public static ArrayList<String> getProgrammingLanguages() {
-        //напишите тут ваш код
-        ArrayList<String> programmingLanguages = new ArrayList<>();
-        programmingLanguages.add("Java");
-        programmingLanguages.add("Kotlin");
-        programmingLanguages.add("Go");
-        programmingLanguages.add("Javascript");
-        programmingLanguages.add("Typescript");
-        programmingLanguages.add("Python");
-        programmingLanguages.add("PHP");
-        programmingLanguages.add("C++");
+    public static HashMap<Integer, String> getProgrammingLanguages() {
+        HashMap<Integer, String>programmingLanguages = new HashMap<>();
+        programmingLanguages.put(0, "Java");
+        programmingLanguages.put(1, "Kotlin");
+        programmingLanguages.put(2, "Go");
+        programmingLanguages.put(3, "Javascript");
+        programmingLanguages.put(4, "Typescript");
+        programmingLanguages.put(5, "Python");
+        programmingLanguages.put(6, "PHP");
+        programmingLanguages.put(7, "C++");
         return programmingLanguages;
     }
 
