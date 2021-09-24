7075703083009b9faecae919366eca150036c777
AndrewSukhov <igrok-cs@yandex.ru>
2021-09-24 21:51:48 +0300

    level 14. enum: перечисление. Четыре сезона.

---
+++ com/javarush/task/pro/task13/task1315/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task13.task1315;
+
+/* 
+Четыре сезона
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
