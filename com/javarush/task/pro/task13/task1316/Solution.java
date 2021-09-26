5d9ae08b50fe2f9534b77fd9305f4acc1b61beea
AndrewSukhov <igrok-cs@yandex.ru>
2021-09-26 21:12:29 +0300

    level 14. enum: Перечислим квесты Javarush. Решение.

--- com/javarush/task/pro/task13/task1316/Solution.java
+++ com/javarush/task/pro/task13/task1316/Solution.java
@@ -3,6 +3,8 @@ package com.javarush.task.pro.task13.task1316;
 public class Solution {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (JavarushQuest value : JavarushQuest.values()) {
+            System.out.println(value.ordinal());
+        }
     }
 }
