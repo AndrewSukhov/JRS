7a6e574e358a42e0247051be1d56eb454a1bc385
AndrewSukhov <igrok-cs@yandex.ru>
2021-09-27 17:23:08 +0300

    level 14. enum. Месяцы в сезоне

---
+++ com/javarush/task/pro/task13/task1319/Month.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task13.task1319;
+
+/* 
+Месяцы в сезоне
+*/
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+
+    //напишите тут ваш код
+
+}
