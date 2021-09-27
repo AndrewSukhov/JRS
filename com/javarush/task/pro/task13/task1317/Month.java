0bfcb5ca7adbec64feb7757216763d4961bcfeac
AndrewSukhov <igrok-cs@yandex.ru>
2021-09-27 12:39:51 +0300

    level 14. enum. Трансформируем enum в класс. Решение.

--- com/javarush/task/pro/task13/task1317/Month.java
+++ com/javarush/task/pro/task13/task1317/Month.java
@@ -4,17 +4,38 @@ package com.javarush.task.pro.task13.task1317;
 Трансформируем enum в класс
 */
 
-public enum Month {
-    JANUARY,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER
+import java.awt.event.MouseAdapter;
+
+public class Month {
+    public static final Month JANUARY = new Month(0);
+    public static final Month FEBRUARY = new Month(1);
+    public static final Month MARCH = new Month(2);
+    public static final Month APRIL = new Month(3);
+    public static final Month MAY = new Month(4);
+    public static final Month JUNE = new Month(5);
+    public static final Month JULY = new Month(6);
+    public static final Month AUGUST = new Month(7);
+    public static final Month SEPTEMBER = new Month(8);
+    public static final Month OCTOBER = new Month(9);
+    public static final Month NOVEMBER = new Month(10);
+    public static final Month DECEMBER = new Month(11);
+
+    private static final Month[] ARRAY = {JANUARY, FEBRUARY,
+            MARCH, APRIL, MAY, JUNE, JULY,
+            AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+    };
+
+    private final int value;
+
+    private Month(int number) {
+        this.value = number;
+    }
+
+    public static Month[] values() {
+        return ARRAY;
+    }
+
+    public int ordinal() {
+        return value;
+    }
 }
