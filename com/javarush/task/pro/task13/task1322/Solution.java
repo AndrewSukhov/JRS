527eccbe329178293d65f5a9cd19199b6b1ab561
AndrewSukhov <igrok-cs@yandex.ru>
2021-09-29 12:52:14 +0300

    level 14. switch. Типы многоугольников. Решение.

--- com/javarush/task/pro/task13/task1322/Solution.java
+++ com/javarush/task/pro/task13/task1322/Solution.java
@@ -9,22 +9,47 @@ public class Solution {
     }
 
     public static String getShapeNameByCountOfCorners(int countOfCorner) {
-        String shape;
-        if (countOfCorner == 3) {
-            shape = "Треугольник";
-        } else if (countOfCorner == 4) {
-            shape = "Четырехугольник";
-        } else if (countOfCorner == 5) {
-            shape = "Пятиугольник";
-        } else if (countOfCorner == 6) {
-            shape = "Шестиугольник";
-        } else if (countOfCorner == 7) {
-            shape = "Семиугольник";
-        } else if (countOfCorner == 8) {
-            shape = "Восьмиугольник";
-        } else {
-            shape = "Другая фигура";
-        }
-        return shape;
+        return switch (countOfCorner) {
+            case 3 -> "Треугольник";
+            case 4 -> "Четырехугольник";
+            case 5 -> "Пятиугольник";
+            case 6 -> "Шестиугольник";
+            case 7 -> "Семиугольник";
+            case 8 -> "Восьмиугольник";
+            default -> "Другая фигура";
+        };
     }
 }
+/* второй вариант
+       String shape = switch (countOfCorner) {
+            case 3 -> "Треугольник";
+            case 4 -> "Четырехугольник";
+            case 5 -> "Пятиугольник";
+            case 6 -> "Шестиугольник";
+            case 7 -> "Семиугольник";
+            case 8 -> "Восьмиугольник";
+            default -> "Другая фигура";
+        };
+        return shape;
+    }
+
+ третий вариант решения
+    switch(countOfCorner)
+    {
+        case 3:
+            return "Треугольник";
+        case 4:
+            return "Четырехугольник";
+        case 5:
+            return "Пятиугольник";
+        case 6:
+            return "Шестиугольник";
+        case 7:
+            return "Семиугольник";
+        case 8:
+            return "Восьмиугольник";
+        default:
+            return "Другая фигура";
+    }
+}
+*/
