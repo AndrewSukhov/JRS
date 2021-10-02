a381af1561b448808ff5498c623c33a91f967ef0
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-02 17:50:22 +0300

    level 15. Исключения. Оборачивание исключений.

---
+++ com/javarush/task/pro/task14/task1410/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task14.task1410;
+
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystemException;
+
+/* 
+Оборачивание исключений
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        copyFile("book.txt", "book_final_copy.txt");
+        copyFile("book_final_copy.txt", "book_last_copy.txt");
+    }
+
+    static void copyFile(String sourceFile, String destinationFile) {
+        FileUtils.readFile(sourceFile);
+        FileUtils.writeFile(destinationFile);
+        //напишите тут ваш код
+    }
+}
