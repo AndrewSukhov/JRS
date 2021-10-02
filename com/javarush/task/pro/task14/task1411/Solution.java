17f5af5d1984f35d130e062502094c6d0e174d70
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-02 18:12:43 +0300

    level 15. Исключения. Распаковка исключений.

---
+++ com/javarush/task/pro/task14/task1411/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task14.task1411;
+
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystemException;
+
+/* 
+Распаковка исключений
+*/
+
+public class Solution {
+
+    public static final String FAILED_TO_READ = "Не удалось прочесть файл.";
+    public static final String FAILED_TO_WRITE = "Не удалось записать в файл.";
+
+    public static FileManager fileManager = new FileManager();
+
+    public static void main(String[] args) {
+        fileManager.copyFile("book.txt", "book_final_copy.txt");
+        fileManager.copyFile("book_final_copy.txt", "book_last_copy.txt");
+        //напишите тут ваш код
+    }
+}
