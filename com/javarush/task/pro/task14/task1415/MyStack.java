f66ba85ba94f4af7962730eb3b3663c7beea96a8
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-03 18:14:14 +0300

    level 15. Исключения. StackTrace. Стек в домашних условиях. Решение.

--- com/javarush/task/pro/task14/task1415/MyStack.java
+++ com/javarush/task/pro/task14/task1415/MyStack.java
@@ -12,22 +12,22 @@ public class MyStack {
     private final List<String> storage = new ArrayList<>();
 
     public void push(String s) {
-        //напишите тут ваш код
+        storage.add(0, s);
     }
 
     public String pop() {
-        //напишите тут ваш код
+        return storage.remove(0);
     }
 
     public String peek() {
-        //напишите тут ваш код
+        return storage.get(0);
     }
 
     public boolean empty() {
-        //напишите тут ваш код
+        return storage.isEmpty();
     }
 
     public int search(String s) {
-        //напишите тут ваш код
+        return storage.indexOf(s);
     }
 }
