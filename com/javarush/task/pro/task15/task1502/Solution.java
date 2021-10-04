4adb02f5648745906bf2a6ab4712300648024faa
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-04 21:28:52 +0300

    level 16. Потоки ввода-вывода. try-with-resources. Возврат к истокам. Решение.

--- com/javarush/task/pro/task15/task1502/Solution.java
+++ com/javarush/task/pro/task15/task1502/Solution.java
@@ -8,11 +8,16 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(System.in);
             String line = scanner.nextLine();
             System.out.println(line.toUpperCase());
         } catch (Exception e) {
             System.out.println("Something went wrong : " + e);
+        } finally {
+            if (scanner != null)
+                scanner.close();
         }
     }
 }
