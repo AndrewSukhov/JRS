45f95a1de9511db96cbad041eb7894c857b9bf90
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-05 17:47:20 +0300

    level 15. Потоки ввода-вывода. Что-то не копируется... Решение.

--- com/javarush/task/pro/task15/task1505/Solution.java
+++ com/javarush/task/pro/task15/task1505/Solution.java
@@ -14,11 +14,10 @@ public class Solution {
              var inputStream = Files.newInputStream(Paths.get(scanner.nextLine()));
              var outputStream = Files.newOutputStream(Paths.get(scanner.nextLine()))
         ) {
-            int size = 1024;
-            byte[] buffer = new byte[size];
+            byte[] buffer = new byte[1024];
             while (inputStream.available() > 0) {
-                int read = inputStream.read();
-                outputStream.write(buffer, size, read);
+                int read = inputStream.read(buffer);
+                outputStream.write(buffer, 0, read);
             }
         } catch (Exception e) {
             System.out.println("Something went wrong : " + e);
