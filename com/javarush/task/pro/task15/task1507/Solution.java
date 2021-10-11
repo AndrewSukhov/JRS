e703c85fa047d430a3c5c72a3734292e64c6858f
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-11 15:43:41 +0300

    level 15. Потоки ввода-вывода. Пропускаем не всех. Решение.

--- com/javarush/task/pro/task15/task1507/Solution.java
+++ com/javarush/task/pro/task15/task1507/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.pro.task15.task1507;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,7 +13,15 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        Path path = Paths.get(fileName);
+        List<String> lines = Files.readAllLines(path);
+        for (int i = 0; i < lines.size(); i++) {
+            if (i % 2 == 0) {
+                System.out.println(lines.get(i));
+            }
+        }
     }
 }
 
