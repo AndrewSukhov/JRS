2d36609d85a70ee31aa4b6aee254d886a8f1eeeb
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-11 14:06:32 +0300

    level 15. Потоки ввода-вывода. Фейсконтроль. Решение (вариант с forEach).

--- com/javarush/task/pro/task15/task1506/Solution.java
+++ com/javarush/task/pro/task15/task1506/Solution.java
@@ -13,14 +13,14 @@ public class Solution {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             List<String> lines = Files.readAllLines(Paths.get(scanner.nextLine()));
-            for (String str : lines) {
+            lines.forEach(str -> {
                 char[] chars = str.toCharArray();
                 for (char character : chars) {
                     if (character != ' ' && character != '.' && character != ',') {
                         System.out.print(character);
                     }
                 }
-            }
+            });
         } catch (Exception e) {
             System.out.println("Something went wrong : " + e);
         }
