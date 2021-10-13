0e151503a3ea9684a775382435220663934f0cf6
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-13 20:19:52 +0300

    level 16. Потоки ввода-вывода. Пишем байты в файл. Решение.

--- com/javarush/task/pro/task15/task1510/Solution.java
+++ com/javarush/task/pro/task15/task1510/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.pro.task15.task1510;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -14,13 +13,9 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) throws IOException {
         byte[] bytes = args[0].getBytes();
-        try (InputStream stream = System.in;
+        InputStream stream = System.in;
              Scanner scanner = new Scanner(stream);
-             BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(scanner.nextLine()))) {
-            bufferedWriter.write(bytes);
-        } catch (IOException e) {
-            System.out.println("Something went wrong : " + e);
-        }
+             Files.write(Path.of(scanner.nextLine()), bytes);
     }
 }
 
