b14c8a90581a183bf2cd204d2cb7737d9c39ace6
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-15 18:15:08 +0300

    level 16. Потоки ввода-вывода. Пишем символы в файл. Решение.

--- com/javarush/task/pro/task15/task1511/Solution.java
+++ com/javarush/task/pro/task15/task1511/Solution.java
@@ -7,7 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
 
-/* 
+/*
 Пишем символы в файл
 */
 
@@ -15,12 +15,12 @@ public class Solution {
     public static void main(String[] args) {
         char[] chars = args[0].toCharArray();
         try (InputStream stream = System.in;
-             Scanner scanner = new Scanner(stream)) {
-            Files.write(Path.of(scanner.nextLine()), chars);
+             Scanner scanner = new Scanner(stream);
+             BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(scanner.nextLine()))) {
+            bufferedWriter.write(chars);
         } catch (IOException e) {
             System.out.println("Something went wrong : " + e);
         }
     }
 }
-
 
