8c8816d931b8ce2a90d4db401af981cf00641942
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-18 17:20:51 +0300

    level 16. Потоки ввода-вывода. Все относительно. Решение.

--- com/javarush/task/pro/task15/task1514/Solution.java
+++ com/javarush/task/pro/task15/task1514/Solution.java
@@ -12,7 +12,17 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         String str1 = scanner.nextLine();
         String str2 = scanner.nextLine();
-        //напишите тут ваш код
+        Path path1 = Path.of(str1);
+        Path path2 = Path.of(str2);
+        try {
+            System.out.println(path1.relativize(path2));
+        } catch (Exception e) {
+            try {
+                System.out.println(path2.relativize(path1));
+            } catch (Exception ignore) {
+
+            }
+        }
     }
 }
 
