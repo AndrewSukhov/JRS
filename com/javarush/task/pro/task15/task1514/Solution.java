8dcc71edc37f02c6022b80daea84b791661c0155
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-18 16:57:21 +0300

    level 16. Потоки ввода-вывода. Все относительно.

---
+++ com/javarush/task/pro/task15/task1514/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task15.task1514;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Все относительно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+        //напишите тут ваш код
+    }
+}
+
