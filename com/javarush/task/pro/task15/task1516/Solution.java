954900355b3957bc8c6c1b6f06f107cf69fefc42
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-19 22:00:18 +0300

    level 16. Потоки ввода-вывода. Файл или директория.

---
+++ com/javarush/task/pro/task15/task1516/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task15.task1516;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Файл или директория
+*/
+
+public class Solution {
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
+
