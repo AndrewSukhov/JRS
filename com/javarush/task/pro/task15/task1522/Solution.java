a2b1c09fe58e1b9a1220a1e9227771be5b0f03f6
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-24 21:07:31 +0300

    level 16. Потоки ввода-вывода. Получение информации по API. Решение.

--- com/javarush/task/pro/task15/task1522/Solution.java
+++ com/javarush/task/pro/task15/task1522/Solution.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
-/* 
+/*
 Получение информации по API
 */
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         URL url = new URL("https://javarush.ru/api/1.0/rest/projects");
-        //напишите тут ваш код
+        InputStream input = url.openStream();
+        byte[] buffer = input.readAllBytes();
+        String str = new String(buffer);
+        System.out.println(str);
     }
 }
