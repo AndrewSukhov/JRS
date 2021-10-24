826f45a6afbc56684a70dfbbcb3589735ce869d4
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-24 21:38:19 +0300

    level 16. Потоки ввода-вывода. Получение информации по API. Решение.

--- com/javarush/task/pro/task15/task1523/Solution.java
+++ com/javarush/task/pro/task15/task1523/Solution.java
@@ -11,7 +11,19 @@ import java.net.URLConnection;
 public class Solution {
     public static void main(String[] args) throws IOException {
         URL url = new URL("http://httpbin.org/post");
-        //напишите тут ваш код
+        URLConnection connection = url.openConnection();
+        connection.setDoOutput(true);
+
+        try (OutputStream output = connection.getOutputStream();
+             PrintStream sender = new PrintStream(output)) {
+            sender.println("Привет");
+        }
+
+        try (InputStream input = connection.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+            while (reader.ready()) {
+                System.out.println(reader.readLine());
+            }
+        }
     }
 }
-
