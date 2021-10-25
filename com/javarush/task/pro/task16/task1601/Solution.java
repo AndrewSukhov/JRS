b23e402fa0229c5d5dfc7fe138300dddc854c0b1
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-25 23:30:58 +0300

    level 17. Работа со временем и датой. Лишь бы не в понедельник :) Решение.

--- com/javarush/task/pro/task16/task1601/Solution.java
+++ com/javarush/task/pro/task16/task1601/Solution.java
@@ -9,15 +9,31 @@ import java.util.Date;
 
 public class Solution {
 
-    static Date birthDate;
+    static Date birthDate = new Date(120, Calendar.MARCH, 17);
 
     public static void main(String[] args) {
         System.out.println(getDayOfWeek(birthDate));
     }
 
     static String getDayOfWeek(Date date) {
-        //напишите тут ваш код
-
-        return null;
+        String result = "";
+        int day = date.getDay();
+        switch (day) {
+            case 0:
+                return "воскресенье";
+            case 1:
+                return "понедельник";
+            case 2:
+                return "вторник";
+            case 3:
+                return "среда";
+            case 4:
+                return "четверг";
+            case 5:
+                return "пятница";
+            case 6:
+                return "суббота";
+        }
+        return result;
     }
 }
