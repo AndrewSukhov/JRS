eed5ad5ba2e41ca4d2902ae6bde2960c0809445c
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-25 17:42:22 +0300

    level 17. Работа со временем и датой. Лишь бы не в понедельник :)

---
+++ com/javarush/task/pro/task16/task1601/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task16.task1601;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/* 
+Лишь бы не в понедельник :)
+*/
+
+public class Solution {
+
+    static Date birthDate;
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(Date date) {
+        //напишите тут ваш код
+
+        return null;
+    }
+}
