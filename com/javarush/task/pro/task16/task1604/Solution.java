ad4ac501f59d044400925e6fe1771b30f8c6e371
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-27 15:21:21 +0300

    level 17. Работа со временем и датой. День недели рождения твоего.

---
+++ com/javarush/task/pro/task16/task1604/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task16.task1604;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/* 
+День недели рождения твоего
+*/
+
+public class Solution {
+
+    static Calendar birthDate;
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(Calendar calendar) {
+        //напишите тут ваш код
+
+        return null;
+    }
+}
