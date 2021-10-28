ba767732bd47888beead765a42aa7e3d859e2d8a
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-28 22:01:01 +0300

    level 17. Работа со временем и датой. День недели рождения твоего. Решение.

--- com/javarush/task/pro/task16/task1604/Solution.java
+++ com/javarush/task/pro/task16/task1604/Solution.java
@@ -9,15 +9,31 @@ import java.util.GregorianCalendar;
 
 public class Solution {
 
-    static Calendar birthDate;
+    static Calendar birthDate = new GregorianCalendar(2020, Calendar.MARCH, 11);
 
     public static void main(String[] args) {
         System.out.println(getDayOfWeek(birthDate));
     }
 
     static String getDayOfWeek(Calendar calendar) {
-        //напишите тут ваш код
-
-        return null;
+        String result = "";
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (day) {
+            case 1:
+                return "воскресенье";
+            case 2:
+                return "понедельник";
+            case 3:
+                return "вторник";
+            case 4:
+                return "среда";
+            case 5:
+                return "четверг";
+            case 6:
+                return "пятница";
+            case 7:
+                return "суббота";
+        }
+        return result;
     }
 }
