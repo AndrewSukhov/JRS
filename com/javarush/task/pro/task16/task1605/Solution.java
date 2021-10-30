578ba99629872bf5d7c5508359ab64fd3a42600e
AndrewSukhov <igrok-cs@yandex.ru>
2021-10-30 22:46:35 +0300

    level 17. Работа со временем и датой. Карманный телепорт.

---
+++ com/javarush/task/pro/task16/task1605/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task16.task1605;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/* 
+Карманный телепорт
+*/
+
+public class Solution {
+
+    public static final String INVENTED = "Пожалуйста, можете забрать ваш карманный телепорт. Спасибо за ожидание!";
+    public static final String NOT_INVENTED = "Извините, телепорт еще не изобрели, приходите через 10 лет.";
+
+    public static final Calendar INVENTION_DAY = new GregorianCalendar(2192, Calendar.JUNE, 12);
+
+    public static void main(String[] args) {
+        Calendar currentDay = Calendar.getInstance();
+        //напишите тут ваш код
+    }
+
+    static boolean isTeleportInvented(Calendar currentDay) {
+        //напишите тут ваш код
+
+        return false;
+    }
+}
