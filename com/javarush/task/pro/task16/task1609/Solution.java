41fc26a9fae3b49bdfd0dd194569719a9cb51cf8
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-02 21:40:20 +0300

    level 17. Работа со временем и датой. С годами моложе. Решение.

--- com/javarush/task/pro/task16/task1609/Solution.java
+++ com/javarush/task/pro/task16/task1609/Solution.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 public class Solution {
 
     public static void main(String[] args) {
-        LocalDate jubilee40Years = LocalDate.of(-2010, 3, 13);
+        LocalDate jubilee40Years = LocalDate.of(2010, 3, 13);
 
         LocalDate jubilee30Years = jubilee40Years.minusYears(10);
 
