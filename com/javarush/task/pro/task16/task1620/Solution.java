62049fd7c9c530bbfa3dd6573adb9b2499d59411
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-13 21:54:42 +0300

    level 17. Работа со временем и датой. Еще один простой шаблон. Решение.

--- com/javarush/task/pro/task16/task1620/Solution.java
+++ com/javarush/task/pro/task16/task1620/Solution.java
@@ -12,6 +12,8 @@ public class Solution {
     static ZonedDateTime zonedDateTime = ZonedDateTime.now();
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("e d.M.yy HH:mm:ss.n VV");
+        String text = dtf.format(zonedDateTime);
+        System.out.println(text);
     }
 }
