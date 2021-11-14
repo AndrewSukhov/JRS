2f55e79e255dc625b4123fa36aa144ac6e6b8393
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-14 19:39:52 +0300

    level 17. Работа со временем и датой. Напиши шаблон.

---
+++ com/javarush/task/pro/task16/task1621/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task16.task1621;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/* 
+Напиши шаблон
+*/
+
+public class Solution {
+
+    static DateTimeFormatter dateTimeFormatter;
+    static String timeString = "13:30:45 23/02/2019";
+
+    public static void main(String[] args) {
+        LocalDateTime dateTime;
+
+        System.out.println(dateTime);
+    }
+}
