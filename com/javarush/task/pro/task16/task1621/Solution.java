f00f5f575373e6215cd12adcf26f37c21f1c6404
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-14 21:20:45 +0300

    level 17. Работа со временем и датой. Напиши шаблон. Решение.

--- com/javarush/task/pro/task16/task1621/Solution.java
+++ com/javarush/task/pro/task16/task1621/Solution.java
@@ -9,11 +9,11 @@ import java.time.format.DateTimeFormatter;
 
 public class Solution {
 
-    static DateTimeFormatter dateTimeFormatter;
+    static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");;
     static String timeString = "13:30:45 23/02/2019";
 
     public static void main(String[] args) {
-        LocalDateTime dateTime;
+        LocalDateTime dateTime = LocalDateTime.parse(timeString, dateTimeFormatter);
 
         System.out.println(dateTime);
     }
