cc3be0fa26c7038826620bcbab9e6b358f6490bb
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-22 20:44:12 +0300

    level 18. Знакомство с ООП. Питомцы бывают разные. Решение.

--- com/javarush/task/pro/task17/task1707/Dog.java
+++ com/javarush/task/pro/task17/task1707/Dog.java
@@ -3,5 +3,8 @@ package com.javarush.task.pro.task17.task1707;
 public class Dog extends Pet {
     public static final String DOG = "Я люблю людей.";
 
-    //напишите тут ваш код
+    public void printInfo() {
+        super.printInfo();
+        System.out.println(DOG);
+    }
 }
