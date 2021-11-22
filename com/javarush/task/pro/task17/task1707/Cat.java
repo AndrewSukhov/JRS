43ff8c348ac6819ac411b68ae0b0f28cc3fe152c
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-22 20:25:34 +0300

    level 18. Знакомство с ООП. Питомцы бывают разные.

---
+++ com/javarush/task/pro/task17/task1707/Cat.java
@@ -0,0 +1,7 @@
+package com.javarush.task.pro.task17.task1707;
+
+public class Cat extends Pet {
+    public static final String CAT = "Я не люблю людей.";
+
+    //напишите тут ваш код
+}
---
+++ com/javarush/task/pro/task17/task1707/Dog.java
@@ -0,0 +1,7 @@
+package com.javarush.task.pro.task17.task1707;
+
+public class Dog extends Pet {
+    public static final String DOG = "Я люблю людей.";
+
+    //напишите тут ваш код
+}
