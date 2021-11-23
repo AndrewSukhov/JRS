85f030009a2fe6ef2657c22d576f433a3192ae1f
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-23 20:03:39 +0300

    level 18. Знакомство с ООП. Минимальное и максимальное. Решение.

--- com/javarush/task/pro/task17/task1708/MinMaxUtil.java
+++ com/javarush/task/pro/task17/task1708/MinMaxUtil.java
@@ -5,5 +5,35 @@ package com.javarush.task.pro.task17.task1708;
 */
 
 public class MinMaxUtil {
-    //напишите тут ваш код
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    public static int min(int a, int b, int c) {
+        return min(c, min(a, b));
+    }
+
+    public static int min(int a, int b, int c, int d) {
+        return min(min(c, d), min(a, b));
+    }
+
+    public static int min(int a, int b, int c, int d, int e) {
+        return min(e, min(min(c, d), min(a, b)));
+    }
+
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public static int max(int a, int b, int c) {
+        return max(c, max(a, b));
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        return max(max(c, d), max(a, b));
+    }
+
+    public static int max(int a, int b, int c, int d, int e) {
+        return max(e, max(max(c, d), max(a, b)));
+    }
 }
