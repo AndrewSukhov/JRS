a75161ec29d128d4013ce24b8120b9d72e1e7cca
AndrewSukhov <igrok-cs@yandex.ru>
2021-11-24 23:10:37 +0300

    level 18. Знакомство с ООП. Абстрактные фигуры. Решение.

--- com/javarush/task/pro/task17/task1709/Shape.java
+++ com/javarush/task/pro/task17/task1709/Shape.java
@@ -4,6 +4,10 @@ package com.javarush.task.pro.task17.task1709;
 Абстрактные фигуры
 */
 
-public class Shape {
-    //напишите тут ваш код
+public abstract class Shape {
+
+    public abstract double getPerimeter();
+
+    public abstract double getArea();
+
 }
