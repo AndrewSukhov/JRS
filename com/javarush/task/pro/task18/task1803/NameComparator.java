12a179e2b3129f050c3186babe86076703751a9c
AndrewSukhov <igrok-cs@yandex.ru>
2022-01-19 12:03:56 +0300

    level 19. Лямбда-функции. Наставники JavaRush. Решение.

--- com/javarush/task/pro/task18/task1803/NameComparator.java
+++ com/javarush/task/pro/task18/task1803/NameComparator.java
@@ -6,6 +6,9 @@ import java.util.Comparator;
 Наставники JavaRush
 */
 
-public class NameComparator {
-    //напишите тут ваш код
+public class NameComparator implements Comparator<JavaRushMentor> {
+    @Override
+    public int compare(JavaRushMentor o1, JavaRushMentor o2) {
+        return o1.getName().length() - o2.getName().length();
+    }
 }
